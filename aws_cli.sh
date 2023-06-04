9fe7e1bce60351c97dcdcddc0638fa805cb509f0
mowahid latif <mowahidlatif@hotmail.com>
2023-06-04 18:25:05 -0500

    Made changes to names and fixed EC2 master and workers

--- aws_cli.sh
+++ aws_cli.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # create-aws-vpc
 #variables used in script:
-availabilityZone="us-east-1a"
-name="your VPC/network name"
+
+availabilityZone="us-east-1"
+name="weclouddata"
 vpcName="$name VPC"
 subnetName="$name Subnet"
 gatewayName="$name Gateway"
@@ -12,9 +13,10 @@ vpcCidrBlock="10.0.0.0/16"
 subNetCidrBlock="10.0.1.0/24"
 port22CidrBlock="0.0.0.0/0"
 destinationCidrBlock="0.0.0.0/0"
-YOUR_AWS_ACCESS_KEY=YOUR_AWS_ACCESS_KEY
-YOUR_AWS_SECRET_ACCESS_KEY=YOUR_AWS_SECRET_ACCESS_KEY
-YOUR_PREFERRED_REGION=YOUR_PREFERRED_REGION
+
+# YOUR_AWS_ACCESS_KEY=YOUR_AWS_ACCESS_KEY
+# YOUR_AWS_SECRET_ACCESS_KEY=YOUR_AWS_SECRET_ACCESS_KEY
+# YOUR_PREFERRED_REGION=YOUR_PREFERRED_REGION
 
 echo "Creating VPC..."
 
@@ -116,64 +118,57 @@ associate_response=$(aws ec2 associate-route-table \
  --subnet-id "$subnetId" \
  --route-table-id "$routeTableId")
 
-# Define your parameters
-imageId=ami-0abcdef1234567890 # replace with a real image ID for your chosen OS
-instanceType=t2.micro
-keyName=my-key-pair
-subnetId=subnet-1a2b3c4d # replace with a real subnet
+# Define your parameters <----------------------------------------------------------------- DELETE THOSE THAT ARE NOT IN USE. 
+# imageId=ami-0abcdef1234567890 # replace with a real image ID for your chosen OS
+# instanceType=t2.micro
+# keyName=my-key-pair
+# subnetId=subnet-1a2b3c4d # replace with a real subnet
 
 ###### and that you have an SSH key pair available for connecting to your instances.
 
 # Launch the master node
-masterInstanceId=$(aws ec2 run-instances --image-id $imageId --count 1 --instance-type $instanceType --key-name $keyName --security-group-ids $groupId --subnet-id $subnetId --query 'Instances[0].InstanceId' --output text)
-echo "Launched master EC2 instance with ID: $masterInstanceId."
+masterInstanceId=$(aws ec2 run-instances \
+--image-id $imageId \
+--count 1 \
+--instance-type $instanceType \
+--key-name $keyName \
+--security-group-ids $groupId \
+--subnet-id $subnetId \
+--user-data "$userData" \
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=worker-node-01}]' \ 
+--query 'Instances[0].InstanceId' \
+--output text)
+echo "Launched Master Node 1 EC2 instance with ID: $masterInstanceId."
 
 # Launch the worker nodes
-for i in {1..2}
-do
-    workerInstanceId=$(aws ec2 run-instances --image-id $imageId --count 1 --instance-type $instanceType --key-name $keyName --security-group-ids $groupId --subnet-id $subnetId --query 'Instances[0].InstanceId' --output text)
-    echo "Launched worker EC2 instance with ID: $workerInstanceId."
+workerInstance1Id=$(aws ec2 run-instances \
+  --image-id $imageId \
+  --count 1 \
+  --instance-type $instanceType \
+  --key-name $keyName \
+  --security-group-ids $groupId \
+  --subnet-id $subnetId \
+  --user-data "$userData" \
+  --query 'Instances[0].InstanceId' \
+  --output text)
 
-    # Assuming you have Docker, kubeadm, kubelet, and kubectl installed on these nodes, join them to the master node
-    joinCommand=$(ssh -i $keyName.pem ubuntu@$masterIdAddress "sudo kubeadm token create --print-join-command")
-    ssh -i $keyName.pem ubuntu@$masterIdAddress "$joinCommand"
+echo "Launched Worker Node 1 EC2 instance with ID: $workerInstance2Id."
 
-######
+workerInstance2Id=$(aws ec2 run-instances \
+  --image-id $imageId \
+  --count 1 \
+  --instance-type $instanceType \
+  --key-name $keyName \
+  --security-group-ids $groupId \
+  --subnet-id $subnetId \
+  --user-data "$userData" \
+  --query 'Instances[0].InstanceId' \
+  --output text)
+
+echo "Launched Worker Node 2 EC2 instance with ID: $workerInstance2Id."
 
 echo " "
 echo "VPC created:"
 echo "Use subnet id $subnetId and security group id $groupId"
 echo "To create your AWS instances"
 # end of create-aws-vpc
-
-
-# Define your parameters
-# IMAGE_ID=ami-0abcdef1234567890 # replace with a real image ID for your chosen OS
-# INSTANCE_TYPE=t2.micro
-# KEY_NAME=my-key-pair
-# SECURITY_GROUP_ID=sg-123abc45d # replace with a real security group
-# SUBNET_ID=subnet-1a2b3c4d # replace with a real subnet
-
-
-
-###### and that you have an SSH key pair available for connecting to your instances.
-
-
-
-# # Launch the master node
-# MASTER_INSTANCE_ID=$(aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type $INSTANCE_TYPE --key-name $KEY_NAME --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID --query 'Instances[0].InstanceId' --output text)
-# echo "Launched master EC2 instance with ID: $MASTER_INSTANCE_ID."
-
-# # Launch the worker nodes
-# for i in {1..2}
-# do
-#     WORKER_INSTANCE_ID=$(aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type $INSTANCE_TYPE --key-name $KEY_NAME --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID --query 'Instances[0].InstanceId' --output text)
-#     echo "Launched worker EC2 instance with ID: $WORKER_INSTANCE_ID."
-
-#     # Assuming you have Docker, kubeadm, kubelet, and kubectl installed on these nodes, join them to the master node
-#     JOIN_COMMAND=$(ssh -i $KEY_NAME.pem ubuntu@$MASTER_IP_ADDRESS "sudo kubeadm token create --print-join-command")
-#     ssh -i $KEY_NAME.pem ubuntu@$WORKER_IP_ADDRESS "$JOIN_COMMAND"
-
-
-
-# ######
